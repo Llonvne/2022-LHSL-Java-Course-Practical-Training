d1604816b0193182bce48a9f1c66d30463a87b4d
2022LHSL <105185815+2022LHSL@users.noreply.github.com>
2022-06-26 10:30:56 +0800

    l4

--- OrderingSystem/src/ui/displayables/AvailableMenuUIDisplay.java
+++ OrderingSystem/src/ui/displayables/AvailableMenuUIDisplay.java
@@ -18,13 +18,14 @@ public class AvailableMenuUIDisplay implements Displayable {
     @Override
     public void display() {
         System.out.println("--------------- 所有可用菜单 ---------------");
-        System.out.println("序号        菜品        描述        单价        类别");
+        System.out.println("序号    菜品        描述        单价     类别");
         for (ImmutableRecord record : new TableGetter("菜品表").getTable()){
-            System.out.printf(record.getAttribute("菜品编号").getValue() + "\n"
-                    + record.getAttribute("菜品名").getValue() + "      "
-                    + record.getAttribute("菜品描述").getValue() + "       "
-                    + record.getAttribute("价格").getValue() + "      "
-                    + record.getAttribute("其他").getValue());
+            System.out.printf("%-6s%-10s%-10s%-6s%-4s\n",
+                    record.getAttribute("菜品编号").getValue(),
+                    record.getAttribute("菜品名").getValue(),
+                    record.getAttribute("菜品描述").getValue(),
+                    record.getAttribute("价格").getValue(),
+                    record.getAttribute("其他").getValue());
         }
         System.out.println("--------------- 祝您用餐愉快！ ---------------");
     }
