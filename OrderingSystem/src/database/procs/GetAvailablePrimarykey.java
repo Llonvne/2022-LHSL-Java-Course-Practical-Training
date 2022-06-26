eab91abe41094cdcd8d438cd28572c31be72569e
Llonvne <work@llonvne.cn>
2022-06-26 13:59:10 +0800

    Create GetAvailablePrimarykey.java

---
+++ OrderingSystem/src/database/procs/GetAvailablePrimarykey.java
@@ -0,0 +1,31 @@
+package database.procs;
+
+import database.sqlTools.AdvanceResultSet;
+import database.sqlTools.QueryExecute;
+
+import java.sql.SQLException;
+
+/**
+ * 类名:     GetAvailablePrimarykey
+ * 描述:
+ * 隶属于:   OrderingSystem
+ * 建立事件： 2022/6/26
+ * 作者：    llonvne
+ * 邮箱：    Work@llonvne.cn
+ * Copyright (c) 2022,All rights reserved.
+ */
+public class GetAvailablePrimarykey {
+    public static int getAvailablePrimarykey(String tableName, String primaryKey) {
+        String sql = "SELECT MAX(CAST(" + primaryKey + " as UNSIGNED)) as pri FROM " + tableName;
+        AdvanceResultSet resultSet = QueryExecute.executeQuery(sql);
+        String id = "";
+        try {
+            while (resultSet.getResultSet().next()) {
+                id = resultSet.getResultSet().getString("pri");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("数据集解析异常");
+        }
+        return Integer.parseInt(id) + 1;
+    }
+}
