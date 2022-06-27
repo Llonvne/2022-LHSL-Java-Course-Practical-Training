eebacb52dc8a27b662e58c46f992da7ec8719c78
2022LHSL <105185815+2022LHSL@users.noreply.github.com>
2022-06-27 14:26:04 +0800

    l10

--- OrderingSystem/src/ui/displayables/PaymentUIDisplay.java
+++ OrderingSystem/src/ui/displayables/PaymentUIDisplay.java
@@ -18,7 +18,7 @@ public class PaymentUIDisplay implements Displayable {
     public void display() {
         System.out.println(">>>请选择你的支付方式");
         for (ImmutableRecord record : new TableGetter("支付方式").getTable()){
-            System.out.println(record.getAttribute("支付方式").getValue() + "\t"
+            System.out.println(record.getAttribute("支付方式") + "\t"
             + record.getAttribute("备注").getValue() + "\t");
         }
     }
