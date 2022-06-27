c8a1271ddebd23514643d2f6331de13620d49c2c
2022LHSL <uni@llonvne.cn>
2022-06-27 16:45:29 +0800

    0

--- OrderingSystem/src/process/Main.java
+++ OrderingSystem/src/process/Main.java
@@ -1,17 +1,10 @@
 package process;
 
-import database.DatabaseHandler;
 import database.DatabaseInitializer;
-import database.commonOperations.DeleteOperations;
-import database.commonOperations.InsertOperations;
-import database.commonOperations.UpdateOperations;
 import exec.recall.Recevier;
 import exec.tasksCenter.StandardTasksCenter;
 import exec.tasksCenter.TasksCenter;
-import process.account.admin.AdminUIDisplay;
 import process.welcome.WelcomeTask;
-import ui.FormHandler;
-import ui.UIOperations.UIOperations;
 
 /**
  * 类名:     process.Main
@@ -28,18 +21,18 @@ public class Main {
     public static final TasksCenter tasksCenter = new StandardTasksCenter();
 
     public static void main(String[] args) {
-//        // 当主任务调度器为空时向主任务调度器添加 Welcome 监听器
-//        Recevier<Boolean> addWelcomeTask = args1 -> tasksCenter.offer(
-//            // 初始化 WelcomeTask 并绑定 主任务接收器
-//            new WelcomeTask(tasksCenter.getTaskRecevier())
-//        );
-//        // 将监听器绑定到主任务调度器
-//        tasksCenter.addTasksEmptyEventListener(addWelcomeTask);
-//        // 想任务中心添加初始化数据库的任务
-//        tasksCenter.offer(new DatabaseInitializer("OrderingSystem"));
-//        // 执行任务中心
-//        tasksCenter.exec();
-        new DatabaseInitializer("OrderingSystem").exec();
-        new InsertOperations(DatabaseHandler.getInstance().getDatabaseHandler().getTable("菜品表")).exec();
+        // 当主任务调度器为空时向主任务调度器添加 Welcome 监听器
+        Recevier<Boolean> addWelcomeTask = args1 -> tasksCenter.offer(
+            // 初始化 WelcomeTask 并绑定 主任务接收器
+            new WelcomeTask(tasksCenter.getTaskRecevier())
+        );
+        // 将监听器绑定到主任务调度器
+        tasksCenter.addTasksEmptyEventListener(addWelcomeTask);
+        // 想任务中心添加初始化数据库的任务
+        tasksCenter.offer(new DatabaseInitializer("OrderingSystem"));
+        // 执行任务中心
+        tasksCenter.exec();
+//        new DatabaseInitializer("OrderingSystem").exec();
+//        new InsertOperations(DatabaseHandler.getInstance().getDatabaseHandler().getTable("菜品表")).exec();
     }
 }
